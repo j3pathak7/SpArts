98979b601981ce325bbba5688b00374845d5ca0c
Jyotirmoy Pathak <jyotirmoypathak.37@gmail.com>
2023-12-24 22:52:39 +0530

    .

--- src/components/home/div5/Div5.tsx
+++ src/components/home/div5/Div5.tsx
@@ -1,7 +1,12 @@
-import { CiFilter } from "react-icons/ci";
-import { TbDots } from "react-icons/tb";
+import React from 'react';
+import { CiFilter } from 'react-icons/ci';
+import { TbDots } from 'react-icons/tb';
 
-const DynamicTable = ({ columns, data }) => {
+interface DynamicTableProps {
+  data: string[][];
+}
+
+const DynamicTable: React.FC<DynamicTableProps> = ({ data }) => {
   return (
     <table>
       <tbody>
@@ -22,33 +27,36 @@ const DynamicTable = ({ columns, data }) => {
   );
 };
 
-// Example usage
-const columns = ['Column 1', 'Column 2', 'Column 3', 'Column 4', 'Column 5', 'Column 6', 'Column 7'];
-const sampleData = [
-  ['Row 1 Data 1', 'Row 1 Data 2', 'Row 1 Data 3', 'Row 1 Data 4', 'Row 1 Data 5', 'Row 1 Data 6', 'Row 1 Data 7'],
-  ['Row 2 Data 1', 'Row 2 Data 2', 'Row 2 Data 3', 'Row 2 Data 4', 'Row 2 Data 5', 'Row 2 Data 6', 'Row 2 Data 7'],
-  // Add more rows as needed
-];
+interface Div5Props {
+  // Define any props your component might take
+}
 
-const Div5 = () => {
+const Div5: React.FC<Div5Props> = () => {
+  const columns = ['Column 1', 'Column 2', 'Column 3', 'Column 4', 'Column 5', 'Column 6', 'Column 7'];
+  const sampleData = [
+    ['Row 1 Data 1', 'Row 1 Data 2', 'Row 1 Data 3', 'Row 1 Data 4', 'Row 1 Data 5', 'Row 1 Data 6', 'Row 1 Data 7'],
+    ['Row 2 Data 1', 'Row 2 Data 2', 'Row 2 Data 3', 'Row 2 Data 4', 'Row 2 Data 5', 'Row 2 Data 6', 'Row 2 Data 7'],
+    // Add more rows as needed
+  ];
+
   return (
     <div className="flex flex-col">
       <div className='flex justify-between border-b border-gray-300 items-center'>
         <div className='p-4'>
-        UPCOMING SCHEDULE
+          UPCOMING SCHEDULE
         </div>
-            <div className="flex text-sm text-white items-center">
-                 <CiFilter size={24} style={{ color: 'black' }} />
-        <button className='bg-gray-600  m-2 p-2 rounded-xl w-24'>All</button>
-        <button className='bg-gray-400 m-2 p-2 rounded-xl w-24'>Classes</button>
-        <button className='bg-gray-400 m-2 p-2 rounded-xl w-24'>Meetings</button>
-        <button className='bg-gray-400 m-2 p-2 rounded-xl w-24'>Events</button>
+        <div className="flex text-sm text-white items-center">
+          <CiFilter size={24} style={{ color: 'black' }} />
+          <button className='bg-gray-600  m-2 p-2 rounded-xl w-24'>All</button>
+          <button className='bg-gray-400 m-2 p-2 rounded-xl w-24'>Classes</button>
+          <button className='bg-gray-400 m-2 p-2 rounded-xl w-24'>Meetings</button>
+          <button className='bg-gray-400 m-2 p-2 rounded-xl w-24'>Events</button>
         </div>
         <div className='m-2 mx-4'>
-            <TbDots size={32} /> 
+          <TbDots size={32} />
         </div>
       </div>
-      <DynamicTable columns={columns} data={sampleData} />
+      <DynamicTable data={sampleData} />
     </div>
   );
 };
